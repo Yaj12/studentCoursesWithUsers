95618725b893911a41c83f68b49de7b596019a76
9p6b <9p6b@fresnou.org>
2022-03-03 08:38:36 -0800

    Done

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,19 @@ var router = express.Router();
 const courseController = require('../controllers/courseController.js');
 const studentController = require('../controllers/studentController.js');
 const userController = require('../controllers/userController.js');
-
+function redirectGuests(req, res, next){
+  if (!req.user){
+    res.redirect('/login');
+    return
+  }
+  next();
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('/courses');
 });
 
-router.get('/courses', courseController.viewAll);
+router.get('/courses', redirectGuests, courseController.viewAll);
 router.get('/courses/profile/:id', courseController.viewProfile);
 router.get('/courses/edit/:id', courseController.renderEditForm);
 router.post('/courses/edit/:id', courseController.updateCourse);
@@ -22,8 +28,6 @@ router.get('/students', studentController.viewAll);
 router.get('/students/profile/:id', studentController.viewProfile);
 router.get('/students/edit/:id', studentController.renderEditForm);
 router.post('/students/edit/:id', studentController.updateStudent);
-router.get('/students/add', studentController.renderAddForm);
-router.post('/students/add', studentController.addStudent);
 router.get('/students/delete/:id', studentController.deleteStudent);
 
 router.post('/students/:studentId/enroll/', studentController.enrollStudent);
